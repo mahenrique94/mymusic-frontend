da7723611bafb0c308eb3939eb0bbfe645f6e39e
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-09-20 23:31:00 -0300

    Sending project to Github

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to otavio-frontend!');
+    expect(page.getParagraphText()).toEqual('Welcome to mymusic-frontend!');
   });
 });
